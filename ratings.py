53e09c94117b1f7e1ad95993df877e6fcc6e108e
keiva <jen@silverwire.ninja>
2021-01-11 17:00:02 -0700

    First Commit

---
+++ ratings.py
@@ -0,0 +1,29 @@
+"""Restaurant rating lister.
+Your job is to write a program named ratings.py that:
+
+    Reads the ratings from the file
+
+    Stores them in a dictionary
+
+    And finally, spits out the ratings in alphabetical order by restaurant
+
+Hint 1: Using .items() to get a list of your dictionary entries will help with sorting.
+
+Hint 2: Refer to the Python docs on the sorted() function if you need a reminder of how to sort.
+"""
+
+
+# define function
+
+
+# open scores.txt file to get data to add to dictionary
+
+# create empty dictionary to store data
+
+# loop through dictionary to view all restaurants
+
+# sort the dictionary items by key (restaurant name)
+
+# return filled, sorted dictionary
+
+# call function
